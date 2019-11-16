852819dc567ba7623481e2495ae1434fe82e005a
treeform <starplant@gmail.com>
2019-11-16 06:13:27 -0800

    Some spacing fixes.

--- src/cairoft.nim
+++ src/cairoft.nim
@@ -1,10 +1,10 @@
 #
-# Translation of cairo-ft.h 
-# by Jeffrey Pohlmeyer 
-# updated to version 1.4 by Luiz Américo Pereira Câmara 2007
+# Translation of cairo-ft.h
+# by Jeffrey Pohlmeyer
+# updated to version 1.4 by Luiz Amï¿½rico Pereira Cï¿½mara 2007
 #
 
-import 
+import
   cairo, freetypeh
 
 include "cairo_pragma.nim"
@@ -20,12 +20,12 @@ include "cairo_pragma.nim"
 # typedef FcPattern XftPattern;
 #
 
-type 
-  FcPattern* = Pointer
+type
+  FcPattern* = pointer
   PFcPattern* = ptr FcPattern
 
-proc ftFontFaceCreateForPattern*(pattern: PFcPattern): PFontFace{.libcairo, importc: "cairo_ft_font_face_create_for_pattern".}
+proc ftFontFaceCreateForPattern*(pattern: PFcPattern): PFontFace {.libcairo, importc: "cairo_ft_font_face_create_for_pattern".}
 proc ftFontOptionsSubstitute*(options: PFontOptions, pattern: PFcPattern){. libcairo, importc: "cairo_ft_font_options_substitute".}
-proc ftFontFaceCreateForFtFace*(face: TFT_Face, load_flags: int32): PFontFace{.libcairo, importc: "cairo_ft_font_face_create_for_ft_face".}
-proc ftScaledFontLockFace*(scaled_font: PScaledFont): TFT_Face{.libcairo, importc: "cairo_ft_scaled_font_lock_face".}
-proc ftScaledFontUnlockFace*(scaled_font: PScaledFont){.libcairo, importc: "cairo_ft_scaled_font_unlock_face".}
+proc ftFontFaceCreateForFtFace*(face: TFT_Face, load_flags: int32): PFontFace {.libcairo, importc: "cairo_ft_font_face_create_for_ft_face".}
+proc ftScaledFontLockFace*(scaled_font: PScaledFont): TFT_Face {.libcairo, importc: "cairo_ft_scaled_font_lock_face".}
+proc ftScaledFontUnlockFace*(scaled_font: PScaledFont) {.libcairo, importc: "cairo_ft_scaled_font_unlock_face".}
